aa30bfbf7d43fbf4252fdb321d0ca4bf9c4343bf
Mitch <mitchchiles@gmail.com>
2019-04-01 17:28:24 -0400

    Export basic memory cache

--- packages/memory-cache/src/main.ts
+++ packages/memory-cache/src/main.ts
@@ -1,2 +1,3 @@
 // Azure document cache
 export { AzureDocumentCacheData, AzureDocumentTableCacheData, AzureTableDocumentCacheInMemory } from './data/azure-document-cache';
+export { BasicMemoryCache } from './data/basic-memory-cache';
