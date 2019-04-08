cd876ee6703e76b6c506945145a2ae2d450c1dc1
Mitch <mitchchiles@gmail.com>
2019-04-08 16:45:57 -0400

    Add blob to main.ts azure table storage

--- packages/azure-table-storage/src/main.ts
+++ packages/azure-table-storage/src/main.ts
@@ -3,3 +3,5 @@ export { IAzureDocumentSavable, AzureDocumentResult,
         IAzureDocumentBatch, AzureDocumentBatch, AzureDocumentBatchResult, AzureDocumentBatchResults,
         AzureDocumentBatchType, AzureDocumentStorageManager, AzureTableDocumentCacheInMemory, AzureDocumentIdentifier, AzureDocumentCacheData, 
         AzureDocumentTableCacheData, IAzureDocumentCache } from './data/azure-document-storagemanager.logic';
+
+export { AzureBlobOperationResult, AzureBlobStorageManager } from './data/azure-blob-storagemanager.logic';
