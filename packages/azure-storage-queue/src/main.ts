3c6bc94086400831dde11137f2f6c60ef09633e2
Mitch <mitchchiles@gmail.com>
2021-03-30 03:13:01 -0400

    Expose classes through main.ts for queue storage manager

---
+++ packages/azure-storage-queue/src/main.ts
@@ -0,0 +1,6 @@
+export { QueueMessage } from './models/queue-message';
+
+export { AzureQueueDocumentResult, AzureQueueDocumentResultWithData } from './models/queue-document-result';
+
+
+export { AzureQueueStorageManager } from './data/azure-queue-storage-manager';
